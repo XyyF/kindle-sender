2e5c4fd1fba3ea4fe09b6345fe310af17534b827
xiaoyufei <915724526@qq.com>
2020-04-01 07:34:45 +0800

    feat(master): build darwin

--- src/renderer/plugins/file-upload.js
+++ src/renderer/plugins/file-upload.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import FileUpload from 'vue-upload-component/src';
+import FileUpload from 'vue-upload-component';
 
 Vue.component('file-upload', FileUpload);
 Vue.filter('formatSize', (size) => {
